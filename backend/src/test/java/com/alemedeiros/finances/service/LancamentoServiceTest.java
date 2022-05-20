f7615093f38399176c6310f854fc9b2133618c01
efactoringseniorlgithub@gmail.com <refactoringsd>
2022-05-20 09:38:07 -0300

    chore: implementado teste de atualizar

--- backend/src/test/java/com/alemedeiros/finances/service/LancamentoServiceTest.java
+++ backend/src/test/java/com/alemedeiros/finances/service/LancamentoServiceTest.java
@@ -55,4 +55,23 @@ public class LancamentoServiceTest {
         catchThrowableOfType(() -> service.salvar(lancamentoSalvar), RegraNegocioException.class);
         Mockito.verify(repository, Mockito.never()).save(lancamentoSalvar);
     }
+
+    @Test
+    void deveAtualizarUmLancamento(){
+        //cenario
+        Lancamentos lancamentoSalvo = LancamentoRepositoryTest.criarLancamento();
+        lancamentoSalvo.setId(1l);
+        lancamentoSalvo.setStatus(StatusLancamento.PENDENTE);
+
+        Mockito.doNothing().when(service).validar(lancamentoSalvo);
+
+        Mockito.when(repository.save(lancamentoSalvo)).thenReturn(lancamentoSalvo);
+
+        //execucao
+        service.atualizar(lancamentoSalvo);
+
+        //verificacao
+        Mockito.verify(repository, Mockito.times(1)).save(lancamentoSalvo);
+    }
+
 }
