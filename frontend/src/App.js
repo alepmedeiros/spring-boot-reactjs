7b5972545d7c1bf932d5ca19a1ed21ce25b93fd5
efactoringseniorlgithub@gmail.com <refactoringsd>
2022-05-22 09:34:52 -0300

    chore: aplicado alguns testes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    nome: ''
+  }
+  render () {
+    return(
+      <div>
+        <label>Nome:</label>
+        <input type="text" value={this.state.nome} onChange={(e) => this.setState({nome: e.target.value})}/>
+        <br/>
+        Digitado: {this.state.nome}
+      </div>
+      )
+  }
 }
 
 export default App;
